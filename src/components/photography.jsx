89b272790dff96d9b94610b9ed8b766e690e945a
Abishek pariyar- portfolio <swaggyabii56@gmail.com>
2025-10-18 18:05:49 -0400

    added popup features

--- src/components/photography.jsx
+++ src/components/photography.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import "./photography.css"
+import { AnimatePresence, motion } from "framer-motion";
 
 const galleryImage = import.meta.glob("/src/assets/galleryimg/*.{jpg,jpeg,png,webp,JPG}",{eager:true});
 const imgUrl = Object.values(galleryImage).map(img=>img.default); 
@@ -6,9 +8,19 @@ const imgUrl = Object.values(galleryImage).map(img=>img.default);
 
 
 export default function Photography(){
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(()=>{
+        if(isClicked){
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = "auto"
+        }
+    })
 
 
-    console.log(imgUrl)
+
+
     return(
         <section className="main-section">
             <div className="top-line-photography">
@@ -17,20 +29,76 @@ export default function Photography(){
                 <a target="_blank" href="https://www.instagram.com/pariyar_abizake/">My Instagram profile</a>
             </div>
             <div className="main-content-photography">
-                <div className="main-gallery">
+                <div  className="main-gallery">
 
                     {imgUrl
                     .slice()
                     .reverse()
                     
                     .map((src,i) =>(
-                        <img  key={i} src={src} alt="`image-${i}"/>
+                        <img
+                        onClick={()=>setIsClicked(src)} 
+                        
+                        
+                        key={i} src={src} alt="`image-${i}"/>
 
                     ))};
 
 
                 </div>
+
+                {isClicked && <div>
+                    <AnimatePresence>
+                        <motion.div
+                        initial={{scale:0.8}}
+                        animate={{scale:1}}
+                        exit={{scale:0.8}}
+                              transition={{ duration: 0.25, ease: "easeOut" }}
+
+                        className="gallery-modal-background"
+
+                        onClick={()=>setIsClicked(null)}
+                    
+                        >
+                            <motion.div
+                            initial={{scale:0.8}}
+                        animate={{scale:1}}
+                        exit={{scale:0.8}}
+                              transition={{ duration: 0.25, ease: "easeOut" }}
+
+                        className="gallery-modal"
+                        onClick={(e)=>e.stopPropagation()}
+                        
+                            
+                            >
+                                 <img src={isClicked} alt="`image-${i}"/>
+                                
+
+
+                            </motion.div>
+                            
+                        </motion.div>
+                    </AnimatePresence>
+
+
+
+
+                </div>
+                    
+                
+
+                
+                
+                
+                
+                
+                
+                
+                
+}
+
             </div> 
+
              
         </section>
     )
